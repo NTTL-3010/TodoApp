36eb4bdf664945da1a962f48ac9c9a0b87d6b0db
Thùy Linh <88276478+NTTL-3010@users.noreply.github.com>
2022-10-24 11:19:39 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,28 +3,22 @@ import './App.css';
 import Noteitem from './Pages/Noteitem';
 import { HomePage } from './Pages/Home';
 import { About } from './Pages/About';
-import { MyContextProvider } from './contexts/MyContext';
-import { TodoContextProvider } from './contexts/TodoContext';
 function App() {
   return (
     <div className="App">
-      <MyContextProvider>
         <BrowserRouter>
           <nav class = "navbar">
             <Link to='/home' className='mylink'>Home</Link>
             <Link to='/about' className='mylink'>About</Link>
             <Link to='/Noteitem' className='mylink'>Todo</Link>
           </nav>
-          <TodoContextProvider>
             <Routes>
               <Route exact path='/' element={<HomePage/>} />
               <Route exact path='/about' element={<About />} />
               <Route exact path='/home' element={<HomePage/>} />
               <Route exact path='/noteitem' element={<Noteitem/>} />
             </Routes>
-          </TodoContextProvider>
         </BrowserRouter>
-      </MyContextProvider>
     </div>
   );
 }
