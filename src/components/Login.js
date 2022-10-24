ce51c034e471aa42fd023d8488e605be917ba66c
Thùy Linh <thhuyliinh3010@gmail.com>
2022-10-24 10:12:11 +0700

    upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,12 @@
-import { useContext, useState } from "react";
-import { MyContext } from '../contexts/MyContext';
+
 
 export const Login = () => {
-    const [user, setUser] = useState('');
-    const { setUsername, setIsLogged } = useContext(MyContext);
+   
 
     return (
         <>
             <h2>LOGIN</h2>
-            <div class="text">
-                Username:
-                <input value={user} onChange={(e) => {
-                    setUser(e.target.value);
-                    setUsername(e.target.value);
-                    setIsLogged(e.target.value !== '');
-                }} />
-                Password:
-                <input value={user} onChange={(e) => {
-                    setUser(e.target.value);
-                    setUsername(e.target.value);
-                    setIsLogged(e.target.value !== '');
-                }} />
-                <button class="_login">Login</button>
-            </div>
+            
         </>
     )
 }
